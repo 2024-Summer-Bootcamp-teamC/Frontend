35e9ca4f8b713ac20ce9822827f2ec2c826be719
kingjinyong <wlsdydwhswkf@gmail.com>
2024-07-05 21:12:21 +0900

    초기 세팅 완료

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,12 @@
-import * as path from "path";
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import svgrPlugin from "vite-plugin-svgr";
+import * as path from 'path';
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react(), svgrPlugin()],
-	server: {
-		port: 3000,
-	},
-	resolve: {
-		alias: {
-			"@": path.resolve(__dirname, "./src"),
-		},
-	},
+  plugins: [react()],
+  server: {
+    port: 3000,
+    host: true,
+  },
 });

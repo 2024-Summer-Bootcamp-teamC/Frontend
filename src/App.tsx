686adddfe928bdacc2055630d1b424ef47852562
yena0213 <yeana3131@naver.com>
2024-07-09 03:26:40 +0900

    [Design] 위인 카드 앞면 구현& 위인앞면 카드 모음 페이지 구현& app.tsx에 NavBar추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import './index.css';
 import NavBar from './components/NavBar';
+import GreatPage from './pages/GreatPage';
 
 const App: React.FC = () => {
-    return (
-      <div className="flex items-center justify-center min-h-screen font-songmyung"></div>
-    );
-
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen font-songmyung">
+      <NavBar />
+      <div className="content"> main 입니당 ~?? </div>
+    </div>
+  );
+};
 
 export default App;
---
+++ src/components/CardFront.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import CardFrontImage from "../assets/images/CardFrontImage.png";
+import Rectangle from "../assets/images/Rectangle.png";
+
+const CardFront: React.FC = () => {
+    return (
+        <div className="relative flex-shrink-0 w-[237.298px] h-[366.962px]">
+            <img 
+                className="w-full h-full"
+                src={CardFrontImage} 
+                alt='Front Of Card' 
+            />
+            <div className="absolute flex flex-col items-center text-black top-3 left-4">
+                <span className="text-lg">세</span>
+                <span className="text-lg">종</span>
+                <span className="text-lg">대</span>
+                <span className="text-lg">왕</span>
+            </div>
+            <div className="absolute inset-0 flex items-center justify-center">
+                <img 
+                    className="w-[154.336px] h-[200.652px] object-cover rounded-lg"
+                    src={Rectangle} 
+                    alt='Front Of Card' 
+                />
+            </div>
+            <div className="absolute flex flex-col items-center text-black transform rotate-180 bottom-4 right-4"> {/* transform rotation으로 회전! */}
+                <span className="text-lg">세</span>
+                <span className="text-lg">종</span>
+                <span className="text-lg">대</span>
+                <span className="text-lg">왕</span>
+            </div>
+        </div>
+    );
+};
+
+export default CardFront;

6c5a7704c88e2ad5d5d93cd59f9e1a22174b043d
choihooo <67588757+choihooo@users.noreply.github.com>
2024-07-08 10:46:41 +0900

    design: App.tsx 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 // src/App.tsx
 import React from 'react';
 import './index.css';
-import ChartVideoBackground from './components/ChartVideoBackground';
 
 const App: React.FC = () => {
   return <div className="flex items-center justify-center min-h-screen font-songmyung"></div>;

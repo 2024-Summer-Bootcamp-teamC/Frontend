a43edb20352e933651ae37e7524f1f55c7d34d38
choihooo <67588757+choihooo@users.noreply.github.com>
2024-07-08 13:50:20 +0900

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,10 @@
 // src/App.tsx
 import React from 'react';
 import './index.css';
-import DetailModal from './components/DetailModal';
 
 const App: React.FC = () => {
   return (
-    <div className="flex items-center justify-center min-h-screen font-songmyung">
-      <DetailModal></DetailModal>
-    </div>
+    <div className="flex items-center justify-center min-h-screen font-songmyung"></div>
   );
 };
 

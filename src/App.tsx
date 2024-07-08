ab5b209d0d840728b75b8fb18e14ca75a7a2c36b
kingjinyong <wlsdydwhswkf@gmail.com>
2024-07-08 16:14:30 +0900

    App.tsx 파일 내가 작업했던 흔적 지우기

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,8 @@
 // src/App.tsx
 import React from 'react';
 import './index.css';
-import GreatChatPageLeft from './pages/GreatChatPageLeft';
-
 const App: React.FC = () => {
-  return (
-    <div className="flex items-center justify-center min-h-screen font-songmyung">
-      <GreatChatPageLeft></GreatChatPageLeft>
-    </div>
-  );
+  return <div className="flex items-center justify-center min-h-screen font-songmyung"></div>;
 };
 
 export default App;

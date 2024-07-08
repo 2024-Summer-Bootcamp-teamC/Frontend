a61e4f25cbebcdc3878e76c05eb91417c6c65c51
yena0213 <yeana3131@naver.com>
2024-07-09 05:29:23 +0900

    [chore] app.tsx 원상복구

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,19 @@
 import React from 'react';
 import './index.css';
 import NavBar from './components/NavBar';
-import Puzzle from './components/Puzzle';
 
 const App: React.FC = () => {
   return (
     <div className="flex items-center justify-center min-h-screen font-songmyung">
       <div className="App">
           <NavBar />
-          <Puzzle />
-
+          <div className="content">
+              <h1 className="">   ┏┯┯┯┯┯┓</h1>
+              <h1 className="">   ┃││∧ ∧│┃살려줘!!</h1>
+              <h1 className="">   ┃│  (≧Д≦)  ┃</h1>
+              <h1 className="">   ┃││ф  ф│┃</h1>
+              <h1 className="">   ┗┷┷┷┷┷┛</h1>
+          </div>
       </div>
     </div>
   );

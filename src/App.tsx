020c6fe6f27431f89885ec7a5a935128a249360d
choihooo <hochoi8621@naver.com>
2024-07-08 20:31:46 +0900

    design: 나라별 페이지 퍼블리싱

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,9 @@
 // src/App.tsx
 import React from 'react';
 import './index.css';
-import MapPage from './pages/MapPage';
+
 const App: React.FC = () => {
-  return (
-    <div className="flex items-center justify-center min-h-screen font-songmyung">
-      <MapPage />
-    </div>
-  );
+  return <div className="flex items-center justify-center min-h-screen font-songmyung">\</div>;
 };
 
 export default App;

fb0d8303cecadaeec964e9eb418ef312b9213e9f
choihooo <hochoi8621@naver.com>
2024-07-08 21:08:49 +0900

    design: 마커 위치 수정 및 이벤트 추가

--- src/pages/MapPage.tsx
+++ src/pages/MapPage.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { ComposableMap, Geographies, Geography, Marker } from 'react-simple-maps';
 
 const geoUrl = '../../public/features.json';
@@ -6,18 +6,28 @@ const geoUrl = '../../public/features.json';
 const markers = [
   {
     markerOffset: -30,
-    name: '서울',
+    name: '대한민국',
     coordinates: [126.978, 37.5665],
   },
-  { markerOffset: -30, name: '미국', coordinates: [-77.0369, 38.9072] },
+  { markerOffset: -30, name: '미국', coordinates: [-100.0369, 38.9072] },
   { markerOffset: -30, name: '네덜란드', coordinates: [4.9041, 52.3676] },
   { markerOffset: 15, name: '프랑스', coordinates: [2.3522, 45] },
   { markerOffset: 15, name: '독일', coordinates: [13.405, 52.52] },
 ];
 
 const MapPage = () => {
+  const [hoveredMarker, setHoveredMarker] = useState(null);
+
+  const handleMarkerMouseEnter = (markerName) => {
+    setHoveredMarker(markerName);
+  };
+
+  const handleMarkerMouseLeave = () => {
+    setHoveredMarker(null);
+  };
+
   return (
-    <div className="flex items-center justify-center overflow-hidden ">
+    <div className="flex items-center justify-center overflow-hidden">
       <ComposableMap
         projection="geoMercator"
         projectionConfig={{
@@ -29,7 +39,12 @@ const MapPage = () => {
           {({ geographies }) => geographies.map((geo) => <Geography key={geo.rsmKey} geography={geo} fill="#594637" />)}
         </Geographies>
         {markers.map(({ name, coordinates, markerOffset }) => (
-          <Marker key={name} coordinates={coordinates}>
+          <Marker
+            key={name}
+            coordinates={coordinates}
+            onMouseEnter={() => handleMarkerMouseEnter(name)}
+            onMouseLeave={handleMarkerMouseLeave}
+          >
             <g
               fill="none"
               stroke="#cacaca"
@@ -41,13 +56,15 @@ const MapPage = () => {
               <circle cx="12" cy="10" r="3" />
               <path d="M12 21.7C17.3 17 20 13 20 10a8 8 0 1 0-16 0c0 3 2.7 6.9 8 11.7z" />
             </g>
-            <text
-              textAnchor="middle"
-              y={markerOffset}
-              style={{ fontFamily: 'font-songmyung', fill: '#ffffff', fontSize: '0.8rem' }}
-            >
-              {name}
-            </text>
+            {hoveredMarker === name && (
+              <text
+                textAnchor="middle"
+                y={markerOffset}
+                style={{ fontFamily: 'font-songmyung', fill: '#ffffff', fontSize: '0.8rem' }}
+              >
+                {name}
+              </text>
+            )}
           </Marker>
         ))}
       </ComposableMap>

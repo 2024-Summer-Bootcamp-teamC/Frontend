255625f4bf5db2e951e745d212be5ff52df40f29
yena0213 <yeana3131@naver.com>
2024-07-08 22:00:53 +0900

    [Desgin] NavBar.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,18 @@
-// src/App.tsx
 import React from 'react';
 import './index.css';
+import NavBar from './components/NavBar';
+
 const App: React.FC = () => {
-  return <div className="flex items-center justify-center min-h-screen font-songmyung"></div>;
+    return (
+      <div className="flex items-center justify-center min-h-screen font-songmyung">
+        <div className="App">
+            <NavBar />
+            <div className="content">
+                <h1 className="mt-40 text-center">Welcome to My Website</h1>
+            </div>
+        </div>
+      </div>
+    );
 };
 
 export default App;

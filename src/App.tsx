937b13abc9d09a6abedf0d4b6cd0c7e88e260fb1
kingjinyong <wlsdydwhswkf@gmail.com>
2024-07-09 17:47:33 +0900

    design: 인물 퀴즈 페이지(우) 구현

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,13 @@ import './index.css';
 import NavBar from './components/NavBar';
 import GreatPage from './pages/GreatPage';
 import GreatQuizPageLeft from './pages/GreatQuizPageLeft';
+import GreatQuizPageRight from './pages/GreatQuizPageRight';
 
 const App: React.FC = () => {
   return (
     <div className="flex items-center justify-center min-h-screen font-songmyung">
       <div className="App">
-        <GreatQuizPageLeft></GreatQuizPageLeft>
+        <GreatQuizPageRight></GreatQuizPageRight>
       </div>
     </div>
   );

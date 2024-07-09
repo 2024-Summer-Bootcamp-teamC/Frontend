7e4045d4effa54a9e43709d63e7d92b0f5252d43
kingjinyong <wlsdydwhswkf@gmail.com>
2024-07-09 17:49:40 +0900

    design: App.tsx 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,11 @@
 // src/App.tsx
 import React from 'react';
 import './index.css';
-import NavBar from './components/NavBar';
-import GreatPage from './pages/GreatPage';
-import GreatQuizPageLeft from './pages/GreatQuizPageLeft';
-import GreatQuizPageRight from './pages/GreatQuizPageRight';
 
 const App: React.FC = () => {
   return (
     <div className="flex items-center justify-center min-h-screen font-songmyung">
-      <div className="App">
-        <GreatQuizPageRight></GreatQuizPageRight>
-      </div>
+      <div className="App"></div>
     </div>
   );
 };

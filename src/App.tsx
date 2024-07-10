75df53a734aedd5a1a9a725347d8b0a4df15f47c
이동현 <louisgood625@naver.com>
2024-07-11 03:40:45 +0900

    Feat:분야 선택화면 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
-// src/App.tsx
 import React from 'react';
 import './index.css';
+import FieldPage from './pages/FieldPage';
+//import MainPage from './pages/MainPage';
 
 const App: React.FC = () => {
-  return <div className="flex items-center justify-center min-h-screen font-songmyung"></div>;
+  return (
+  <div className="flex items-center justify-center min-h-screen font-songmyung">
+      <FieldPage />
+  </div>
+  );
 };
 
 export default App;

566c311d937f193d882ca7c4d9053b188213b62f
yena0213 <yeana3131@naver.com>
2024-07-11 04:06:53 +0900

    [Design] 글자에 커서 올리면 bold&밑줄쳐지도록 구현"

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -10,11 +10,11 @@ const NavBar: React.FC<NavBarProps> = ({ goPage }) => {
         <div className="fixed left-0 right-0 w-full top-14" style={{ height: '1px' }}>
             <nav className="border-b border-black">
                 <div className="container mx-auto">
-                    <div className="fixed top-2 left-6 flex items-center gap-2.5 cursor-pointer" onClick={() => goPage(2)}>
+                    <div className="fixed top-2 left-6 flex items-center gap-2.5 cursor-pointer hover:font-bold hover:underline" onClick={() => goPage(2)}>
                         <img src={Pin} alt="PinImage" />
                         <div style={{ fontSize: '30px' }}> 위인 모음 </div>
                     </div>
-                    <div className="fixed cursor-pointer top-2 right-7" style={{ fontSize: '30px' }} onClick={() => goPage(4)}>
+                    <div className="fixed cursor-pointer top-2 right-7 hover:font-bold hover:underline" style={{ fontSize: '30px' }} onClick={() => goPage(4)}>
                         김진용님
                     </div>
                 </div>

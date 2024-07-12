b6d4e53abfc1fa36de91d6597ba2e6f262bc8b29
이동현 <louisgood625@naver.com>
2024-07-12 16:44:40 +0900

    Design: 분야 페이지 완전 완성

--- src/pages/FieldPage.tsx
+++ src/pages/FieldPage.tsx
@@ -18,37 +18,49 @@ const FieldPage: React.FC = () => {
       <div className="absolute inset-0 flex items-center justify-center">
         <div className="absolute inset-0 z-0 translate-y-1 bg-center bg-cover scale-80" style={{ backgroundImage: `url(${Newbook})` }}></div>
 
-        <div className="relative z-10 grid w-2/3 grid-cols-2 gap-20 left-6">
-          {[
-            { label: '정치', image: PoliticalImage },
-            { label: '경제', image: EconomyImage },
-            { label: '과학', image: ScienceImage },
-            { label: '문화예술', image: ArtsImage }
-          ].map((section) => (
-            <div
-              key={section.label}
-              className="relative flex items-center justify-center p-3 cursor-pointer group"
-              onClick={() => handleSectionClick(section.label)}
-              style={{ width: '540px', height: '300px', pointerEvents: 'fill' }} // 컨테이너 크기 고정, 가로 길이 2배로 증가
-            >
-              <img 
-                src={section.image} 
-                alt={section.label} 
-                className="block object-cover w-full h-full transition-opacity duration-200 opacity-50 group-hover:opacity-100" // 이미지 조정 및 투명도 조정
-              />
-              <img src={Tape} alt="Tape" className="absolute top-3 w-[75%] h-[150%] transform -translate-x-1/2 -translate-y-1/2 left-1/2 pointer-events-none" style={{ opacity: 1 }} />
-              <span className="absolute text-5xl pointer-events-none top-11 left-12" style={{ opacity: 1 }}>{section.label}</span> {/* 위치 조정 */}
-            </div>
-          ))}
-        </div>
-        
-        {/* 뒤로가기 버튼 추가 */}
-        <div className="absolute mb-10 ml-20 left-1 bottom-3">
-          <img
-            src={BackImage}
-            alt="뒤로가기"
-            className="h-20 transition-opacity duration-300 opacity-50 cursor-pointer w-15 hover:opacity-100"
-          />
+        <div className="relative z-10 grid w-full grid-cols-2 gap-20">
+          <div className="flex flex-col items-center justify-center gap-20">
+            {[
+              { label: '정치', image: PoliticalImage },
+              { label: '과학', image: ScienceImage }
+            ].map((section) => (
+              <div
+                key={section.label}
+                className="relative flex items-center justify-center p-3 cursor-pointer group left-11"
+                onClick={() => handleSectionClick(section.label)}
+                style={{ width: '540px', height: '300px', pointerEvents: 'fill' }} // 컨테이너 크기 고정
+              >
+                <img 
+                  src={section.image} 
+                  alt={section.label} 
+                  className="block object-cover w-full h-full transition-opacity duration-200 opacity-50 group-hover:opacity-100" // 이미지 조정 및 투명도 조정
+                />
+                <img src={Tape} alt="Tape" className="absolute top-3 w-[75%] h-[150%] transform -translate-x-1/2 -translate-y-1/2 left-1/2 pointer-events-none" style={{ opacity: 1 }} />
+                <span className="absolute text-5xl pointer-events-none top-11 left-12" style={{ opacity: 1 }}>{section.label}</span> {/* 위치 조정 */}
+              </div>
+            ))}
+          </div>
+          <div className="flex flex-col items-center justify-center gap-20">
+            {[
+              { label: '경제', image: EconomyImage },
+              { label: '문화예술', image: ArtsImage }
+            ].map((section) => (
+              <div
+                key={section.label}
+                className="relative flex items-center justify-center p-3 cursor-pointer group right-11"
+                onClick={() => handleSectionClick(section.label)}
+                style={{ width: '540px', height: '300px', pointerEvents: 'fill' }} // 컨테이너 크기 고정
+              >
+                <img 
+                  src={section.image} 
+                  alt={section.label} 
+                  className="block object-cover w-full h-full transition-opacity duration-200 opacity-50 group-hover:opacity-100" // 이미지 조정 및 투명도 조정
+                />
+                <img src={Tape} alt="Tape" className="absolute top-3 w-[75%] h-[150%] transform -translate-x-1/2 -translate-y-1/2 left-1/2 pointer-events-none" style={{ opacity: 1 }} />
+                <span className="absolute text-5xl pointer-events-none top-11 left-12" style={{ opacity: 1 }}>{section.label}</span> {/* 위치 조정 */}
+              </div>
+            ))}
+          </div>
         </div>
       </div>
     </div>

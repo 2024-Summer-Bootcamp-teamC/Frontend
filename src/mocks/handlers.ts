8b02157b41d81dc8fe549ddc57f5d65c346a60df
choihooo <hochoi8621@naver.com>
2024-07-14 14:58:21 +0900

    feat: 에러 뜨는 거 해결 및 msw 최신 버전 적용 완료

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -15,6 +15,11 @@ type AddCommentResponseBody = {
   message: string;
 };
 
+type UserGetPrams = {
+  user_id: number;
+  correct_cnt: number;
+};
+
 interface User {
   id: number;
   name: string;
@@ -95,7 +100,7 @@ const greats: Great[] = [
   },
 ];
 
-const quiz: Quiz[] = [
+const quizzes: Quiz[] = [
   {
     id: 1,
     story_id: 1,
@@ -125,32 +130,33 @@ export const handlers = [
     return HttpResponse.json();
   }),
   // 위인 전체 리스트 불러오기, 선택한 분야의 위인 목록 불러오기
-  // http.get<GreatGetParams, Great, Great[], '/greats/:user_id'>('/greats/:user_id', ({ params }) => {
-  //   const { user_id } = params;
-  //   const user = users.find((user) => user.id === Number(user_id));
-  //   console.log(greats);
-  //   if (user) {
-  //     return HttpResponse.json(greats);
-  //   } else {
-  //     return HttpResponse.json();
-  //   }
-  // }),
-  http.get<GreatGetParams, Great, Great[] | AddCommentResponseBody, '/greats/:param'>(
-    '/greats/:param',
+  http.get<GreatGetParams, Great, Great[], '/greats/:user_id'>('/greats/:user_id', ({ params }) => {
+    const { user_id } = params;
+    const user = users.find((user) => user.id === Number(user_id));
+    console.log(greats);
+    if (user) {
+      return HttpResponse.json(greats);
+    } else {
+      return HttpResponse.json();
+    }
+  }),
+  http.get<GreatGetParams, Great, Great[] | AddCommentResponseBody, '/greats/:user_id/:param'>(
+    '/greats/:user_id/:param',
     ({ params }) => {
-      const { param } = params;
+      const { param, user_id } = params;
 
-      const user = users.find((user) => user.id === Number(param));
-      if (user) {
-        return HttpResponse.json(greats);
-      }
-
+      const user = users.find((user) => user.id === Number(user_id));
       const filterGreat = greats.filter((great) => great.field === param || great.nation === param);
-      if (filterGreat) {
-        return HttpResponse.json(filterGreat);
-      }
 
-      return HttpResponse.json({ status: 404, message: '위인 또는 사용자 없음' });
+      if (user) {
+        if (filterGreat) {
+          return HttpResponse.json(filterGreat);
+        } else {
+          return HttpResponse.json({ status: 404, message: '위인 없음' });
+        }
+      } else {
+        return HttpResponse.json({ status: 404, message: '사용자 없음' });
+      }
     },
   ),
   http.get<GreatGetParams, Great, Great | AddCommentResponseBody, '/greats/:user_id/:story_id'>(
@@ -170,32 +176,53 @@ export const handlers = [
     },
   ),
   // 퀴즈 퍼즐 저장하기
-  http.put<GreatGetParams, Result, Great | AddCommentResponseBody, '/greats/:user_id/:story_id'>(
-    '/greats/:user_id/:story_id',
-    ({ params, request }) => {
-      const { story_id, user_id } = params;
-      const { puzzleData } = request.body;
+  // id: number;
+  // story_id: number;
+  // user_id: number;
+  // puzzle_cnt: number;
+  // correct_cnt: number;
+  http.put<GreatGetParams, UserGetPrams, AddCommentResponseBody, '/greats/:story_id/puzzles'>(
+    '/greats/:story_id/puzzles',
+    async ({ params, request }) => {
+      const { story_id } = params;
+      const { user_id, correct_cnt } = await request.json();
 
-      const response: Quiz = {
-        id: quiz.length + 1,
+      const puzzleData = result.find((re) => re.story_id === Number(story_id) || re.user_id === user_id);
+
+      const response: Result = {
+        id: result.length + 1,
         story_id: Number(story_id),
-        ...puzzleData,
+        user_id: user_id,
+        correct_cnt: correct_cnt,
+        puzzle_cnt: (puzzleData?.puzzle_cnt ?? 0) + 1,
       };
 
-      quiz.push(response);
+      result.push(response);
 
-      return res(ctx.status(201), ctx.json(response));
+      if (puzzleData) {
+        return HttpResponse.json({ status: 201, message: 'good' });
+      } else {
+        return HttpResponse.json({ status: 404, message: 'bad' });
+      }
     },
   ),
   // 퀴즈 불러오기
-  // http.get<Quiz>('/quizzes/:story_id/', (req, res, ctx) => {
-  //   const { story_id } = req.params;
-  //   const story = quiz.find((quiz) => quiz.story_id === Number(story_id));
+  http.get<GreatGetParams, Result, Quiz | AddCommentResponseBody, '/quizzes/:user_id/:story_id'>(
+    '/quizzes/:user_id/:story_id',
+    ({ params }) => {
+      const { user_id, story_id } = params;
+      const user = users.find((user) => user.id === Number(user_id));
+      const quiz = quizzes.find((quiz) => quiz.story_id === Number(story_id));
 
-  //   if (story) {
-  //     return res(ctx.status(200), ctx.json(story));
-  //   } else {
-  //     return res(ctx.status(404), ctx.json({ error: 'Story not found' }));
-  //   }
-  // }),
+      if (user) {
+        if (quiz) {
+          return HttpResponse.json(quiz);
+        } else {
+          return HttpResponse.json({ status: 404, message: '위인 없음' });
+        }
+      } else {
+        return HttpResponse.json({ status: 404, message: '사용자 없음' });
+      }
+    },
+  ),
 ];

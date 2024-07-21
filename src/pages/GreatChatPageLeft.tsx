b56b402548de4b93478db4e406442e60ea8fca55
kingjinyong <wlsdydwhswkf@gmail.com>
2024-07-21 10:35:27 +0900

    feat: 말하는 영상 변경

--- src/pages/GreatChatPageLeft.tsx
+++ src/pages/GreatChatPageLeft.tsx
@@ -48,12 +48,7 @@ const GreatChatPageLeft = forwardRef<{ playVideo: () => void; pauseVideo: () => 
               />
             ))}
           </div>
-          <video
-            ref={videoRef}
-            className="mb-1 w-[295px] h-[399.5px] rounded-[60px] border-[5px] border-amber-800"
-            loop
-            muted
-          >
+          <video ref={videoRef} className="mb-1 w-[300px] h-[399.5px]  border-amber-800" loop muted>
             <source src={Talking} type="video/mp4" />
             Your browser does not support the video tag.
           </video>

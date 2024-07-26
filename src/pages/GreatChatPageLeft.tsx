11d6bad7340cf6437f3e8bfbaa67b0e1603340c1
yena0213 <yeana3131@naver.com>
2024-07-26 16:37:51 +0900

    [Feat]채팅 인물 사진 대상자들 설정

--- src/pages/GreatChatPageLeft.tsx
+++ src/pages/GreatChatPageLeft.tsx
@@ -1,4 +1,4 @@
-import React, { forwardRef, useImperativeHandle, useRef } from 'react';
+import { forwardRef, useImperativeHandle, useRef } from 'react';
 import EmptyPuzzle from '../assets/images/EmptyPuzzle.png';
 import FilledPuzzle from '../assets/images/Puzzle.png';
 import ArrowBack from '../assets/arrow_back.png';
@@ -11,7 +11,7 @@ interface GreatChatProps {
 
 const GreatChatPageLeft = forwardRef<{ playVideo: () => void; pauseVideo: () => void }, GreatChatProps>(
   ({ movePage }, ref) => {
-    const { name, puzzle_cnt } = useGreatPersonStore();
+    const { name, puzzle_cnt, front_url } = useGreatPersonStore();
     const videoRef = useRef<HTMLVideoElement>(null);
 
     useImperativeHandle(ref, () => ({
@@ -35,7 +35,7 @@ const GreatChatPageLeft = forwardRef<{ playVideo: () => void; pauseVideo: () => 
             movePage(7);
           }}
         >
-          <img src={ArrowBack} alt="인물 페이지로 다시 가기" className="h-12  w-17" />
+          <img src={ArrowBack} alt="인물 페이지로 다시 가기" className="h-12 w-17" />
         </div>
         <div className="flex flex-col items-center justify-center mb-[30px] ">
           <div className="flex">
@@ -48,11 +48,16 @@ const GreatChatPageLeft = forwardRef<{ playVideo: () => void; pauseVideo: () => 
               />
             ))}
           </div>
-          <video ref={videoRef} className="mb-1 w-[300px] h-[399.5px]  border-amber-800" loop muted>
-            <source src={Talking} type="video/mp4" />
-            Your browser does not support the video tag.
-          </video>
-          <div className="text-[26px] mt-2">{name}</div>
+          {name==="이순신" ? (
+              <video ref={videoRef} className="mb-1 w-[300px] h-[399.5px]  border-amber-800" loop muted>
+              <source src={Talking} type="video/mp4" />
+              Your browser does not support the video tag.
+            </video>
+            ): (
+              <img src={front_url} className="mb-1 w-[300px] h-[399.5px]" alt="위인 사진" />
+            )
+          }
+         <div className="text-[26px] mt-2">{name}</div>
         </div>
       </>
     );

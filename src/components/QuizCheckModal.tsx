9f6440aa930eeb90b046bc050cdd879c90061d21
yena0213 <yeana3131@naver.com>
2024-07-29 18:40:41 +0900

    [Feat] 정답체크 애니메이션 추가

--- src/components/QuizCheckModal.tsx
+++ src/components/QuizCheckModal.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import CorrectStamp from '../assets/images/CorrectStamp.png';
 import WrongStamp from '../assets/images/WrongStamp.png';
 
@@ -15,6 +15,20 @@ const QuizCheckModal: React.FC<QuizCheckModalProps> = ({
   onClose,
   onNextQuiz
 }) => {
+    
+    const [fadeOut, setFadeOut] = useState(false);
+    const [fadeIn, setFadeIn] = useState(false);
+
+    useEffect(() => {
+        if (isOpen) {
+          setFadeIn(true); // 모달이 열릴 때 fadeIn 상태를 true로 설정
+          setFadeOut(false); // fadeOut 상태를 false로 설정
+        } else {
+          setFadeIn(false); // 모달이 닫힐 때 fadeIn 상태를 false로 설정
+          setFadeOut(true); // fadeOut 상태를 true로 설정
+        }
+      }, [isOpen]);
+
   useEffect(() => {
     if (isOpen) {
       const timer = setTimeout(() => {
@@ -28,15 +42,15 @@ const QuizCheckModal: React.FC<QuizCheckModalProps> = ({
 
   // 배경 클릭 시, 모달창 사라지도록 하는 기능
   const handleBackgroundClick = () => {
-    onClose();
+    setFadeOut(true); // 배경 클릭 시 fadeOut 상태를 true로 설정
   };
 
   if (!isOpen) return null;
 
   return (
     <div 
-      className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50"
-      onClick={handleBackgroundClick} 
+    className={`fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50 ${fadeIn ? 'fade-in' : ''} ${fadeOut ? 'fade-out' : ''}`}
+    onClick={handleBackgroundClick} 
     >
       <div className="relative" >
         <img

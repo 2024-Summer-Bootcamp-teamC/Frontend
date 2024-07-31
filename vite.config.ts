f0a24b6db1e499c39d708da880e4487dce178b66
kingjinyong <wlsdydwhswkf@gmail.com>
2024-07-31 17:41:30 +0900

    chore: Docker레포 를 위해서

--- vite.config.ts
+++ vite.config.ts
@@ -13,6 +13,7 @@ export default defineConfig({
         target: 'http://localhost:8000/api',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ''),
+        ws: true,
       },
     },
   },

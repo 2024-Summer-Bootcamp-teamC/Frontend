71c3fb22fd0e4b693e2b90a27d7743dc2fa48947
박근채 <pgc0419@gmail.com>
2024-08-01 02:40:33 +0900

    Fix: hmr 443포트 제거

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 3000,
+    port: 3000, // Vite가 사용하는 포트
     host: '0.0.0.0',
     proxy: {
       '/api': {
@@ -19,9 +19,8 @@ export default defineConfig({
       },
     },
     hmr: {
-      protocol: 'wss',
+      protocol: 'ws', // HTTPS와는 별도로 ws 프로토콜 사용
       host: 'weinjeon.com',
-      port: 443,  // Port 설정 추가
     },
   },
 });
